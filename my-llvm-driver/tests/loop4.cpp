119717d3db1cc92c5e800a96d4a727b57cb37011
chuqingG <wendy0307@mail.ustc.edu.cn>
2021-01-14 22:17:57 +0800

    add loop4 analysis

--- my-llvm-driver/tests/loop4.cpp
+++ my-llvm-driver/tests/loop4.cpp
@@ -3,6 +3,9 @@ int main(){
     while(i == 1){
         do{
             i++;
+            for(int j = 0;j < 10;j++){
+                j++;
+            }
         L1:
             for(;i>8;i++){
                 i++;
@@ -10,6 +13,8 @@ int main(){
         }while(i<10);
         if(i<5)
             goto L1;
+        for(int k = 0;k < 6;k++)
+            k++;
     }
     return 0;
 }

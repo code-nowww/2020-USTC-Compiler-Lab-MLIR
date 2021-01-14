03f10f1cf0714a3f49077c0a743e356382832a79
FireFlyBurn <wjc2428028720@mail.ustc.edu.cn>
2021-01-14 21:25:59 +0800

    tests

---
+++ my-llvm-driver/tests/loop5.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+        for(int i=0;i<0;i++)
+        {
+                while(0)
+                        ;
+                do
+                        ;
+                while(0);
+                for(;;)
+                        ;
+        }
+}
+
+
